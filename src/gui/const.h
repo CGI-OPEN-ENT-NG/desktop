55c57f210d10306cf18af0962ea1f382293b738e
Nikolaï AMOSSE <nikolai.93@outlook.com>
2024-08-20 15:41:22 +0200

    feat: #DRIV force skip update check

--- src/gui/const.h
+++ src/gui/const.h
@@ -5,10 +5,11 @@
 #include <QDir>
 #include <QStandardPaths>
 
-const QString SERVER_URL = QStringLiteral("https://nextcloud01-dev.support-ent.fr/");
+const QString SERVER_URL = QStringLiteral("https://nextcloud-cd13-prep.support-ent.fr/");
 const QString LOCAL_DIR = QDir::toNativeSeparators(
     QString("%1/eduprovence").arg(QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation))
 );
 const bool AUTO_UPDATE_CHECK = false;
+const bool SKIP_UPDATE_CHECK = true;
 
 #endif
