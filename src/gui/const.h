49dbba5725ab506c6689e1dde6765038576f6210
Nikolaï AMOSSE <nikolai.93@outlook.com>
2024-08-22 14:05:08 +0200

    feat: #DRIV add consts

--- src/gui/const.h
+++ src/gui/const.h
@@ -5,7 +5,11 @@
 #include <QDir>
 #include <QStandardPaths>
 
-const QString SERVER_URL = QStringLiteral("https://nextcloud-cd13-prep.support-ent.fr/");
+// URL DEV  --> 	https://nextcloud01-dev.support-ent.fr/
+// URL RE7  -->     https://nextcloud-re7.support-ent.fr/
+// URL PREP --> 	https://nextcloud-cd13-prep.support-ent.fr/
+
+const QString SERVER_URL = QStringLiteral("https://nextcloud01-dev.support-ent.fr/");
 const QString LOCAL_DIR = QDir::toNativeSeparators(
     QString("%1/eduprovence").arg(QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation))
 );
