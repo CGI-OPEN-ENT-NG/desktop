97316411533b0b98940c257964cf686c0a8147bc
Nikolaï AMOSSE <nikolai.93@outlook.com>
2024-08-23 11:45:30 +0200

    feat: #DRIV-131 block sharing access

--- src/gui/const.h
+++ src/gui/const.h
@@ -9,7 +9,7 @@
 // URL RE7  -->     https://nextcloud-re7.support-ent.fr/
 // URL PREP --> 	https://nextcloud-cd13-prep.support-ent.fr/
 
-const QString SERVER_URL = QStringLiteral("https://nextcloud-cd13-prep.support-ent.fr/");
+const QString SERVER_URL = QStringLiteral("https://nextcloud01-dev.support-ent.fr/");
 const QString LOCAL_DIR = QDir::toNativeSeparators(
     QString("%1/Documents Synchronisés").arg(QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation))
 );
@@ -17,5 +17,7 @@ const bool AUTO_UPDATE_CHECK = false;
 const bool SKIP_UPDATE_CHECK = true;
 
 const QString CONTEXT_MENU_TITLE = QStringLiteral("Espace collégien Provence");
+
+const bool SHARING_AVAILABLE = false;
 
 #endif
