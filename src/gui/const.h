da13e40f68270f714be033a3009c43d73fcac1a9
Nikolaï AMOSSE <nikolai.93@outlook.com>
2024-08-27 08:43:48 +0200

    feat: #DRIV-132 change sync file location to Documents

--- src/gui/const.h
+++ src/gui/const.h
@@ -9,9 +9,15 @@
 // URL RE7  -->     https://nextcloud-re7.support-ent.fr/
 // URL PREP --> 	https://nextcloud-cd13-prep.support-ent.fr/
 
+// Lien vers /Documents/Documents Synchronisés 
+// QDir::toNativeSeparators(
+//     QString("%1/Documents Synchronisés").arg(QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation))
+// );
+
 const QString SERVER_URL = QStringLiteral("https://nextcloud01-dev.support-ent.fr/");
+
 const QString LOCAL_DIR = QDir::toNativeSeparators(
-    QString("%1/Documents Synchronisés").arg(QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation))
+    QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation)
 );
 const bool AUTO_UPDATE_CHECK = false;
 const bool SKIP_UPDATE_CHECK = true;
